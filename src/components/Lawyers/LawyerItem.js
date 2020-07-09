ad67fd3c40d2d1652c1dce5cb2c2ba61bf2885ea
yairazaria <yairazz12@gmail.com>
2020-07-09 20:15:57 +0300

    lawyer item task done

---
+++ src/components/Lawyers/LawyerItem.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import image from './userpic.jpeg';
+import Rating from '../generl/Rating';
+import LawyerSkils from './LawyerSkils';
+
+
+const LawyerItem = () => {
+    return (
+        <div className="container">
+        <div className="row" >
+            <div className="col-md-4">
+    		    <div className="card profile-card-2">
+                    <div className="card-img-block">
+                        <img className="img-fluid" src="https://images.pexels.com/photos/877695/pexels-photo-877695.jpeg?auto=compress&cs=tinysrgb&h=350" alt="Card image cap" />
+                    </div>
+                    
+                    <div className="card-body">
+                        <img src={image}alt="profile-image" className="profile"/>
+                        <div className="lawyer-details-card">
+                    <ul className="lawyer-details_ul">
+                        <li className="lawyer-name" ><a href="#" style={{color: 'white'}}>יוסי עזריה</a></li>
+                        <li className="lawyer-know"> עו״ד לדיני עבודה </li>
+                        <Rating />
+                    </ul>
+                    </div>
+                    </div>
+
+                <p style={{textAlign:'right',fontSize:'x-small',padding: '8px 25px 0px 0px'}}>טקסא על העורך דין</p>
+                <hr/>
+                <ul className="">
+                        <li>דמי הבראה</li>
+                        <li>פנסיה</li>
+                        <li>נסיעות</li>
+                        <li>דמי הבראה</li>
+                        <li>פנסיה</li>
+                        <li>נסיעות</li>
+                </ul>
+                <hr/>
+                    <div>
+                        <div className="d-flex justify-content-between" style={{fontSize:'small'}}>
+                        <div className=" "><i className="fas fa-map-marker-alt" style={{border:'0px', color:'#08b9e3'}}></i>מאיפה</div>
+                        <div className=" " style={{marginLeft: '.8rem'}}>ירושלים</div>
+                        </div>
+                        <div className="d-flex justify-content-between" style={{fontSize:'small'}}>
+                        <div className=""><i class="fas fa-clock" style={{border:'0px', color:'#08b9e3'}}></i>ניסיון בתחום</div>
+                        <div className="" style={{marginLeft: '.8rem'}}>8 שנים</div>
+                        </div>
+                        <div className="d-flex justify-content-between" style={{fontSize:'small'}}>
+                        <div className=" "><i className="fas fa-comment-dots" style={{border:'0px', color:'#08b9e3'}}></i>זמן תגובה ממוצע</div>
+                        <div className=" " style={{marginLeft: '.8rem'}}>3 שעות</div>
+                        </div>
+                        <div className="d-flex justify-content-between" style={{fontSize:'small'}}>
+                        <div className=" "><i className="fas fa-calendar-check" style={{border:'0px', color:'#08b9e3'}}></i>עבודה מכונה תוך זמן</div>
+                        <div className=" " style={{marginLeft: '.8rem'}}>10 ימי עסקים</div>
+                        </div>
+                    </div>
+                    <div className="d-flex justify-content-between p-5" >
+                    <button type="button" className="btn btn-primary" style={{borderColor: 'rgb(8, 185, 227)',backgroundColor:'rgb(8, 185, 227)'}}>צור קשר</button>
+                    <button type="button" className="btn btn-outline-primary offer-btn" style={{color: 'rgb(8, 185, 227)',borderColor:'rgb(8, 185, 227)'}}>לקבלת הצעה</button>
+                    </div>
+                </div>
+    		</div>
+            <LawyerSkils />
+          </div>
+        </div>
+    )
+}
+
+export default LawyerItem;
---
+++ src/components/general/Rating.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const style = {
+    fontSize: 'small',
+    color:'orange'
+};
+const Rating = () => {
+  return (
+    <div style={{display: 'flex', alignItems: 'baseline'}}>
+        <div>
+            <span className="fa fa-star" style={style}></span>
+            <span className="fa fa-star" style={style}></span>
+            <span className="fa fa-star" style={style}></span>
+            <span className="fa fa-star" style={style}></span>
+            <span className="fa fa-star" style={style}></span>
+        </div>
+        <div>
+        <p style={{fontSize: 'x-small'}}>( 12 תגובות )</p>
+        </div>
+    </div>
+  );
+};
+
+export default Rating;
