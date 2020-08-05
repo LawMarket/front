bededc871a93ec1c9a2236952e00d1e68d6cffdc
Yair Azaria <44365672+yairazaria@users.noreply.github.com>
2020-08-05 18:14:07 +0300

    first deployment

---
+++ build/precache-manifest.8356bda54126a38768b7263e58a3e61f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0c767d3abbdcd6bee589cc58345d18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0132106eecc3398f3e3e",
+    "url": "/static/css/main.49be34f5.chunk.css"
+  },
+  {
+    "revision": "045a5db960feaf61df2c",
+    "url": "/static/js/2.d7afb2dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d7afb2dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0132106eecc3398f3e3e",
+    "url": "/static/js/main.22b52852.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "aebae1cb65d453c6961a4f9b63dc6b1f",
+    "url": "/static/media/product.aebae1cb.jpg"
+  }
+]);
